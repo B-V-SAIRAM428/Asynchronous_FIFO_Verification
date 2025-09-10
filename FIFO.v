0fde36b8e1ef09952d8b014726b34bdb0bc9a679
Venkata Sairam <165484111+B-V-SAIRAM428@users.noreply.github.com>
2025-09-10 15:40:08 +0530

    Update FIFO.v

--- FIFO.v
+++ FIFO.v
@@ -1,23 +1,4 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 27.06.2025 18:38:06
-// Design Name: 
-// Module Name: FIFO
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 module FIFO # ( parameter integer wr_width = 16,
                       parameter integer rd_width = 8,
                       parameter integer depth= 512,
@@ -30,77 +11,80 @@ module FIFO # ( parameter integer wr_width = 16,
     input rd_clk,
     input rd_rst,
     input rd_en,
-    output reg full,
-    output reg empty,
+    output wire full,
+    output wire empty,
     output reg [rd_width-1:0] rd_data,
     output reg rd_valid
     );
-    reg [8:0] wr_addr, rd_addr;
-    integer i;
+    localparam addr_width = $clog2(depth);
+    reg [addr_width:0] wr_addr, rd_addr;
+    wire [addr_width:0] wr_addr_b2g;
+    wire [addr_width:0] rd_addr_b2g;
+    reg [addr_width:0] wr_addr_b2g_syn1,wr_addr_b2g_syn2;
+    reg [addr_width:0] rd_addr_b2g_syn1,rd_addr_b2g_syn2;
     reg [width-1:0] mem [depth-1:0] ;
     integer w,r;
     localparam k1= wr_width / width;   
     localparam k2= rd_width / width;  
     
+    
+    // Binary to Gray Convertion
+    assign wr_addr_b2g = (wr_addr>>1) ^ wr_addr;
+    assign rd_addr_b2g = (rd_addr>>1) ^ rd_addr;
+    
+    always@(posedge rd_clk) begin
+        if(rd_rst) begin
+            wr_addr_b2g_syn1 <= 0;
+            wr_addr_b2g_syn2 <= 0;
+        end else begin 
+            wr_addr_b2g_syn1 <= wr_addr_b2g;
+            wr_addr_b2g_syn2 <= wr_addr_b2g_syn1;
+        end
+    end
+    always@(posedge wr_clk) begin
+        if(wr_rst) begin
+            rd_addr_b2g_syn1 <= 0;
+            rd_addr_b2g_syn2 <= 0;
+        end else begin 
+            rd_addr_b2g_syn1 <= rd_addr_b2g;
+            rd_addr_b2g_syn2 <= rd_addr_b2g_syn1;
+        end
+    end
+    
+    assign full = (wr_addr_b2g =={~rd_addr_b2g_syn2[addr_width],rd_addr_b2g_syn2[addr_width-1:0]});
+    assign empty = (wr_addr_b2g_syn2[addr_width:0] == rd_addr_b2g[addr_width:0]) ;
+ 
+
+
     // write operation
     always@(posedge wr_clk) begin
         if (wr_rst) begin
-            for (i=0; i<512; i=i+1) 
-                mem[i] <= 0;
             wr_addr <= 0;
-            
-            full <=0;
-        end else if (wr_addr - rd_addr == depth - k1 && wr_en) begin
+        end else if (wr_en && !full && (wr_addr + k1 <= depth)) begin
             for (w=0; w<k1; w=w+1) begin
                 mem[wr_addr+w] <= wr_data[(w+1)*width-1 -: width];
             end
             wr_addr <= wr_addr+k1;
-            full <= 1;
-        end else if (rd_addr - wr_addr == k1 && wr_en) begin
-            for (w=0; w<k1; w=w+1) begin
-                mem[wr_addr+w] <= wr_data[(w+1)*width-1 -: width];
-            end
-            wr_addr <= wr_addr+k1;
-            full <= 1;
-        end else if (!full && wr_en) begin
-            for (w=0; w<k1; w=w+1) begin
-                mem[wr_addr+w] <= wr_data[(w+1)*width-1 -: width];
-            end
-            wr_addr <= wr_addr+k1;
-            full <= 0;
-       end else if ( wr_addr == rd_addr && rd_en)
-                full <= 0;
        end
-       
+   end
+    
+    
+    
        // read operation
        always@(posedge rd_clk) begin
         if (rd_rst) begin
             rd_addr <= 0;
             rd_data <= 0;
-            empty <=1;
             rd_valid <= 0;
-        end else if (wr_addr - rd_addr  == depth-k2 && rd_en) begin
+        end else if (!empty && rd_en && (rd_addr + k2 <= depth)) begin
             for (r=0; r<k2; r=r+1) begin
                 rd_data[(r+1)*width-1 -: width] <= mem[rd_addr+r];
             end
             rd_addr <= rd_addr+k2;
             rd_valid <=1;
-            empty <= 1;
-        end else if (wr_addr - rd_addr == k2 && rd_en) begin
-            for (r=0; r<k2; r=r+1) begin
-                rd_data[(r+1)*width-1 -: width] <= mem[rd_addr+r];
-            end
-            rd_addr <= rd_addr+k2;
-            empty <= 1;
-            rd_valid <=1;
-        end else if (!empty && rd_en) begin
-            for (r=0; r<k2; r=r+1) begin
-                rd_data[(r+1)*width-1 -: width] <= mem[rd_addr+r];
-            end
-            rd_addr <= rd_addr+k2;
-            empty <= 0;
-            rd_valid <=1;
-       end else if ( wr_addr == rd_addr && wr_en)
-            empty <= 0;
        end
+       else 
+            rd_valid <=0;
+       end
+       
 endmodule
